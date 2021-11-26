d7e7fc65c9bd3ab46fe4fd3a89747b523b619aa4
Nrittik Sarmah <nrittik43@gmail.com>
2021-11-26 15:07:31 +0530

    implemeted local storage

--- Diary/script.js
+++ Diary/script.js
@@ -5,7 +5,7 @@ const button = document.querySelector('button');
 const popup = document.querySelector('.popup-wrapper');
 const popupClose = document.querySelector('.popup-close');
 const search = document.querySelector('.search-entry input');
-
+let key=0;
 const generateTemplate = (body) => {
     const template = `
     <div class="entry">
@@ -15,31 +15,62 @@ const generateTemplate = (body) => {
     <span class="material-icons delete" style="font-size: 36px; position: relative; top: 15px; left: 5px">
         delete
     </span>
-</div>
+    </div>
     `;
     entryList.innerHTML += template;
 }
+// console.log(entryList.children);
+// const entries = Array.from(entryList.children);
+const todos = JSON.parse(localStorage.getItem('todos'));
 
+// entries.forEach(entry => {
+//     todos.push(entry.childNodes[1].innerText);
+// })
+// localStorage.setItem('todos', JSON.stringify(todos));
+
+
+// const storageValues = JSON.parse(localStorage.getItem('todos'));
+// console.log(storageValues);
+// storageValues.forEach(value=> {
+//     generateTemplate(value);
+// })
+JSON.parse(localStorage.getItem('todos')).forEach(value=> {
+    generateTemplate(value);
+})
 newEntry.addEventListener('submit', e=>{
     e.preventDefault();
     const entryText = newEntry.new_entry.value.trim();   
     if(entryText.length){
-        generateTemplate(entryText);
+        todos.push(entryText);
+        localStorage.setItem('todos', JSON.stringify(todos));
         newEntry.new_entry.value = "";
+        console.log(todos);
     }
+    JSON.parse(localStorage.getItem('todos')).forEach(value=> {
+        if(entryText===value) generateTemplate(value);
+    })
 })
 
 entryList.addEventListener('click', e=> {
-    console.log(e);
+    // console.log(e);
     let node = e.target.parentNode;
     if(e.target.classList.contains('delete')){
         popup.style.display = 'block';
         button.onclick = ()=>{
             popup.style.display = 'none';
-            node.remove()
+            // node.remove()
+            console.log(node.childNodes[1].textContent.trim());
+            let i=0;
+            todos.forEach(todo => {
+                if(todo===node.childNodes[1].textContent.trim()){
+                    todos.splice(i, 1);
+                }
+                i++;
+            })
+            localStorage.setItem('todos', JSON.stringify(todos));
+            location.reload();
         }
     }
-    
 })
 
 
